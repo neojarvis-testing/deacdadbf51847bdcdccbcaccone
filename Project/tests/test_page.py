6e285d3295b655749fd8b83514ffd69af3e160fa
neojarvis <techadmin@iamneo.ai>
2025-06-16 10:58:08 +0000

    Auto-save-commit-6/16/2025--4:28:00-PM

--- Project/tests/test_page.py
+++ Project/tests/test_page.py
@@ -1,9 +1,12 @@
 import pytest
 from base import BaseTest
+import time
+from selenium.webdriver.common.by import By
+
 
 class Test_Page(BaseTest):
     def setUp(self):
-        url = 
+        url = "https://www.houseofindya.com/"
         self.driver = self.setUpDriver()
         self.driver.get(url)
         self.driver.maximize_window()
@@ -13,4 +16,22 @@ class Test_Page(BaseTest):
 
     @pytest.mark.smoke
     def test_method_name(self):
-        //Write your methods here
+        # Write your methods here
+        
+        kids = self.driver.find_element()
+        kids.click()
+
+        self.driver.refresh()
+        time.sleep(2)
+
+        locator = self.driver.find_element(By.CSS_SELECTOR, "a[href=\"/store-locator\"]")
+        locator.click()
+
+        self.driver.refresh()
+        time.sleep(2)
+# //a[@href='/women-lehenga-set/cat'][normalize-space()='Lehengas']
+        Lehengas = self.driver.find_element(By.CSS_SELECTOR, "a[href='/menswear']")
+        Lehengas.click()
+
+        time.sleep(2)
+
